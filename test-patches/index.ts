feb6bbe51ec34310887cbdaeac1d25b7493975da
Sébastien Piquemal <sebpiq@gmail.com>
2020-03-23 17:22:01 +0100

    added code to load test patches

---
+++ test-patches/index.ts
@@ -0,0 +1,25 @@
+import path from 'path'
+import fs from 'fs'
+const TEST_PATCHES_DIR = path.resolve(__dirname)
+  
+export default {
+    subpatches: fs
+        .readFileSync(path.join(TEST_PATCHES_DIR, 'subpatches.pd'))
+        .toString(),
+    simple: fs
+        .readFileSync(path.join(TEST_PATCHES_DIR, 'simple.pd'))
+        .toString(),
+    nodeElems: fs
+        .readFileSync(path.join(TEST_PATCHES_DIR, 'node-elems.pd'))
+        .toString(),
+    arrays: fs
+        .readFileSync(path.join(TEST_PATCHES_DIR, 'arrays.pd'))
+        .toString(),
+    graphs: fs
+        .readFileSync(path.join(TEST_PATCHES_DIR, 'graphs.pd'))
+        .toString(),
+    objectSizePdVanilla: fs
+        .readFileSync(path.join(TEST_PATCHES_DIR, 'object-size-pd-vanilla.pd'))
+        .toString(),
+}
+  
