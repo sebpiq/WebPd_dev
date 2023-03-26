fab735c3c38af0c8244d4a2c71ac2489f4310a2a
Sébastien Piquemal <sebpiq@protonmail.com>
2023-03-26 18:30:48 +0200

    Added webpd repo in build script

--- scripts/build-watch.sh
+++ scripts/build-watch.sh
@@ -8,4 +8,5 @@ cd ../WebPd_compiler ; npm run build:assemblyscript-wasm-bindings -- --watch &
 cd ../WebPd_compiler ; echo src/engine-assemblyscript/core-code/fs.asc | entr npm run build:js-engine &
 cd ../WebPd_pd-parser ; npm run build:dist -- --watch &
 cd ../WebPd_runtime ; npm run build:dist -- --watch &
-cd ../WebPd_runtime ; echo src/WebPdWorkletProcessor.ts | entr npm run build:worklet-processor
+cd ../WebPd_runtime ; echo src/WebPdWorkletProcessor.ts | entr npm run build:worklet-processor & 
+cd ../WebPd ; npm run build:dist -- --watch
