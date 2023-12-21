addc8e60b63343c5c6162e103187ca3e4349282d
Sébastien Piquemal <sebpiq@protonmail.com>
2023-12-21 16:46:56 +0100

    tweaked build system

--- configs/dist.rollup.mjs
+++ configs/dist.rollup.mjs
@@ -4,36 +4,12 @@ import { visualizer } from 'rollup-plugin-visualizer'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { string } from 'rollup-plugin-string'
-import fs from 'fs'
 import path from 'path'
 
-const GENERATED_TS_CONFIG_PATH = './tsconfig-generated-for-rollup.json'
-
+const TS_CONFIG_PATH = path.resolve('configs', 'dist.tsconfig.json')
 const DIST_DIR = 'dist'
-
-// Didn't manage to use a shared tsconfig file for rollup, which would live
-// in a separate repo (WebPd_dev), so we're generating it here using a template.
-const makeTsConfig = (options) => `{
-    "compilerOptions": {
-        "module": "ES2022",
-        "target": "ES2021",
-        "esModuleInterop": true,
-        "removeComments": false,
-        "noImplicitAny": true,
-        "preserveConstEnums": true,
-        "sourceMap": ${JSON.stringify(options.sourcemap)},
-        "declaration": true,
-        "declarationDir": "${path.resolve(DIST_DIR, 'types')}",
-        "resolveJsonModule": true
-    },
-    "typeRoots": ["./node_modules/@types"],
-    "include": [
-        "index.ts",
-        "*.d.ts",
-        "src",
-        "@types/**/*.d.ts"
-    ],
-}`
+const TMP_DIR = 'tmp'
+const INPUT_PATH = path.resolve('src', 'index.ts')
 
 export const buildRollupConfig = (options = {}) => {
     options.preserveModules = options.preserveModules !== undefined
@@ -42,9 +18,7 @@ export const buildRollupConfig = (options = {}) => {
     
     options.sourcemap = options.sourcemap !== undefined
         ? options.sourcemap
-        : true
-
-    fs.writeFileSync(GENERATED_TS_CONFIG_PATH, makeTsConfig(options))
+        : false
 
     let plugins = [
         options.importAsString
@@ -52,19 +26,19 @@ export const buildRollupConfig = (options = {}) => {
                   include: options.importAsString,
               })
             : null,
-        typescript({ tsconfig: GENERATED_TS_CONFIG_PATH }),
+        typescript({ tsconfig: TS_CONFIG_PATH }),
         nodeResolve(),
         commonjs(),
         json(),
         visualizer({
             template: 'network',
-            filename: './tmp/rollup-stats.html',
+            filename: path.resolve(TMP_DIR, 'rollup-stats.html'),
         }),
     ]
     plugins = plugins.filter((plugin) => !!plugin)
 
     return {
-        input: 'index.ts',
+        input: INPUT_PATH,
         output: {
             format: 'esm',
             dir: DIST_DIR,
--- configs/jest.js
+++ configs/jest.js
@@ -5,7 +5,6 @@ export default {
     testEnvironment: 'node',
     rootDir: process.cwd(),
     moduleNameMapper: {
-        '^([./a-zA-Z0-9$_-]+)\\.asc$': './__mock__/$1.asc.ts',
         '^([./a-zA-Z0-9$_-]+)\\.js.txt$': './__mock__/$1.js.ts',
     },
     extensionsToTreatAsEsm: ['.ts'],
